c891567ddf9e9222b14fb33dc01e828fbbec53ad
Angelo Belcastro <angelo.belcastro01@gmail.com>
2024-04-29 15:05:17 +0200

    Updated waybar + other stuff

--- dots/.config/hypr/scripts/WaybarLaunch.sh
+++ dots/.config/hypr/scripts/WaybarLaunch.sh
@@ -11,7 +11,7 @@
 #                                                                               #
 #################################################################################
 
-CONFIG_FILES="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/style.css"
+CONFIG_FILES="$HOME/.config/waybar/config.jsonc $HOME/.config/waybar/modules.jsonc $HOME/.config/waybar/style.css"
 
 trap "killall waybar" EXIT
 
