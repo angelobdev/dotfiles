9b746e87675a26a7615e47f4959ec733cc46d42e
angelobdev <angelo.belcastro01@gmail.com>
2024-10-07 14:18:36 +0200

    Initial Commit

---
+++ hypr/.config/hypr/launchers/cliphist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#################################################################################
+#                                                                               #
+#   This script has been written by angelobdev                                  #
+#   It should be used in a properly configured Hyprland environment.            #
+#                                                                               #
+#   Look at the full configuration here: https://github.com/angelobdev/dotfiles #
+#                                                                               #
+#   The project (as well as this script) goes under the GNU GPLv3 LICENSE.      #
+#                                                                               #
+#################################################################################
+
+cliphist list | wofi --dmenu --allow-images -a -i --prompt "Search..." | cliphist decode | wl-copy
---
+++ waybar/.config/waybar/scripts/Switcher.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#################################################################################
+#                                                                               #
+#   This script has been written by angelobdev                                  #
+#   It should be used in a properly configured Hyprland environment.            #
+#                                                                               #
+#   Look at the full configuration here: https://github.com/angelobdev/dotfiles #
+#                                                                               #
+#   The project (as well as this scripts) goes under the GNU GPLv3 LICENSE.     #
+#                                                                               #
+#################################################################################
+
+# *** Parsing sinks ***
+sinks=$(wpctl status)
+
+# Getting sinks lines
+sinks=$(sed -n "/Sinks/, /Sources/ { /Sinks/! { /Sources/! p } }" <<< "$sinks")
+
+# Removing ASCII tree chars
+sinks=$(sed -E "s/[├─└│]+//" <<< "$sinks")
+
+# Removing spaces
+sinks=$(awk '{$1=$1};1' <<< "$sinks")
+
+# Removing volume
+sinks=$(sed -E "s/\[vol: ([0-1]\.[0-9]{2})\]//" <<< "$sinks")
+
+# Creating dictionary
+declare -A sinks_dict
+while IFS= read line; do
+
+    # Getting sink device
+    device=$(cut -d '.' -f2 <<< "$line")
+    device="${device:1}" # (Removing first space)
+
+    # Getting sink ID
+    id=$(cut -d '.' -f1 <<< "$line")
+
+    # Checking if current default
+    if [[ "${id:0:1}" == '*' ]]; then
+        device="<b>  $device</b>"
+        id=${id:2} # Removing * char
+    fi
+
+    # Adding sink to dictionary
+    sinks_dict["$device"]="$id"
+    
+done <<< "$sinks"
+
+# Getting sorted wofi output...
+output=$(
+    for device in "${!sinks_dict[@]}"; do
+        echo "${sinks_dict[$device]}:$device"
+    done | sort -n | cut -d ':' -f2
+)
+
+# ... and its number of lines
+n_lines=$(wc -l <<< "$output")
+
+# Creating wofi choice menu
+killall wofi 2> /dev/null
+choice=$(echo "$output" | wofi \
+                            --show=dmenu \
+                            --hide-scroll \
+                            --allow-markup \
+                            --sort-order=default \
+                            --location=top_right \
+                            --width=30% \
+                            --lines=$((n_lines+1)) \
+                            --xoffset=-30 \
+                            --yoffset=10 \
+                            --prompt="Choose the device..." \
+                            /
+)
+
+# Setting default sink
+if [[ $choice ]]; then
+    sink_id="${sinks_dict[$choice]}"
+    echo "Setting output to $choice with ID: $sink_id"
+    wpctl set-default $sink_id
+fi
