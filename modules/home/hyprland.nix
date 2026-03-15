4260d32ffa922eeecc53db1e3f68bbab2c274e3b
Angelo Belcastro <angelo.belcastro01@gmail.com>
2026-03-16 00:04:49 +0100

    another other fix

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -7,7 +7,7 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    systemd.enable = true;
+    systemd.enable = false;
     package = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     plugins = [
