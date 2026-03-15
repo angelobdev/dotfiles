89b39f2fcc2c9d8c367d5ee362fd71faa698611f
Angelo Belcastro <angelo.belcastro01@gmail.com>
2026-03-15 21:51:53 +0100

    enabled plymouth

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,6 +1,34 @@
 { pkgs, ... }:
 {
-  boot.loader.systemd-boot.enable      = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages                  = pkgs.linuxPackages_latest;
+  boot = {
+    loader.systemd-boot.enable      = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelPackages                  = pkgs.linuxPackages_latest;
+
+    plymouth = {
+      enable = true;
+      # theme = "rings";
+      # themePackages = with pkgs; [
+      #   # By default we would install all themes
+      #   (adi1090x-plymouth-themes.override {
+      #     selected_themes = [ "rings" ];
+      #   })
+      # ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+    
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+
+  };
 }
