dc8fb441e73ddc3a2eaa5d08bdebca44dc16225e
Angelo Belcastro <angelo.belcastro01@gmail.com>
2026-03-16 02:06:47 +0100

    fix fprintd

--- modules/system/display.nix
+++ modules/system/display.nix
@@ -25,6 +25,15 @@
   security.pam.services.sddm.fprintAuth = true;
   security.pam.services.hyprlock.fprintAuth = true;
 
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id.indexOf("net.reactivated.fprint.device.") === 0 &&
+          subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.sddm.enableGnomeKeyring = true;
   security.pam.services.login.enableGnomeKeyring = true;
