e677c8647d58e95dfb3ebf7fa05f1104bb20dbfb
Angelo Belcastro <angelo.belcastro01@gmail.com>
2026-03-16 10:08:44 +0100

    fix: bash

--- config/waybar/scripts/sinkSwitch.sh
+++ config/waybar/scripts/sinkSwitch.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 #################################################################################
 #                                                                               #
 #   This script has been written by angelobdev                                  #
